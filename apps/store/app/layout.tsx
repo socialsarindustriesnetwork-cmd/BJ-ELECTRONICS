b777f3b0c8e7883e3b80770bc6727eb1677285f3
SAR <social.sarindustriesnetwork@gmail.com>
2026-07-26 22:37:09 +0600

    feat(store): load marketplace secondary-page styles

--- apps/store/app/layout.tsx
+++ apps/store/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { getStoreUrl } from "@bje/config";
 import "./globals.css";
 import "./marketplace.css";
+import "./marketplace-pages.css";
 
 const storeUrl = getStoreUrl();
 
